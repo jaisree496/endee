493faa4bc7cb7ba5af34c87135c0a31a9b00f3dd
jaisree496 <js1874348@gmail.com>
2026-04-14 01:53:30 +0530

    Delete embeddings.py

--- embeddings.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# Load model once
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def get_embedding(text):
-    return model.encode(text).tolist()
